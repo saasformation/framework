c088cb7796e3708a2ecc7def4a7a12e8024b844d
Alex Hernández <alex.hernandez@saasformation.com>
2024-11-29 08:34:07 +0100

    fix: logger initialization

--- project/tests/projects/Infrastructure/SymfonyContainerProviderTest.php
+++ project/tests/projects/Infrastructure/SymfonyContainerProviderTest.php
@@ -5,6 +5,7 @@ namespace SaaSFormation\Framework\Tests\Projects\Infrastructure;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
 use SaaSFormation\Framework\Contracts\Infrastructure\EnvVarsManagerInterface;
 use SaaSFormation\Framework\Contracts\Infrastructure\KernelInterface;
 use SaaSFormation\Framework\Projects\Infrastructure\SymfonyContainerProvider;
@@ -22,10 +23,12 @@ class SymfonyContainerProviderTest extends TestCase
         $kernel = $this->createMock(KernelInterface::class);
         /** @var EnvVarsManagerInterface&MockObject $envVarsManager */
         $envVarsManager = $this->createMock(EnvVarsManagerInterface::class);
+        /** @var LoggerInterface&MockObject $logger */
+        $logger = $this->createMock(LoggerInterface::class);
         $symfonyContainerProvider = new SymfonyContainerProvider(__DIR__ . "/../Resources/services.yaml");
 
         // Act
-        $symfonyContainerBuilder = $symfonyContainerProvider->provide($kernel, $envVarsManager);
+        $symfonyContainerBuilder = $symfonyContainerProvider->provide($kernel, $envVarsManager, $logger);
 
         // Assert
         $this->assertInstanceOf(ContainerBuilder::class, $symfonyContainerBuilder);
